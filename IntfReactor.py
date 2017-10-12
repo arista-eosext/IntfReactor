be4e064426839fe85dab75f15a48a595e41e6e01
Jeremy Georges <archjeb@users.noreply.github.com>
2017-10-11 19:02:09 -0700

    Update IntfReactor.py

--- IntfReactor.py
+++ IntfReactor.py
@@ -11,7 +11,7 @@ list of interfaces in which you can execute a script and pass the interface and 
 The executed script when an interface changes status, must NOT be blocking.
 
 Add the following configuration snippets to change the default behavior. For the list
-of IPs to check, separate with a comma.
+of interfaces to check, separate with a comma.
 
 daemon IntfReactor
    exec /mnt/flash/IntfReactor.py
